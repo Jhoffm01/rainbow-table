273bf189075c8f6296d808ffd04e9e476e454212
Jhoffm01 <129583016+Jhoffm01@users.noreply.github.com>
2024-07-04 22:41:12 -0600

    Cleaned up, added pointers, and prepared generators for multithreading
    Organized GuessSingle but it is not at its final stage. Generators mostly done but need to be set up with wait groups for multithreading. Will work on workers next, multithreading will come last

--- HashCracker/brute/brute.go
+++ HashCracker/brute/brute.go
@@ -10,30 +10,40 @@ type PassTest struct {
 	inc     int
 	nums    []int
 	current []rune
+	pipe    *chan string
 }
 type inter interface {
 	//converts []rune from passTest struct into a string, returns string.
 	toString() string
 	//will get the next password to test for passTest
-	getNext([]rune)
+	getNext(*[]rune)
 	//this is ti initiate passTest structures.
-	pTinit(int, int, int, []rune) PassTest
+	pTinit(int, int, int, *[]rune, *chan string) PassTest
 	//this is for guessing a single hash.
 	GuessSingle(string) string
 }
 
-func (x PassTest) getNext(rns []rune) {
-	max := len(rns)
+func (x PassTest) produce(passwords int, rns *[]rune) {
+	for i := 0; i < passwords; i++ {
+		*x.pipe <- x.toString()
+		x.getNext(rns)
+		//needs removed
+		fmt.Println(<-*x.pipe)
+	}
+}
+
+func (x PassTest) getNext(rns *[]rune) {
+	max := len(*rns)
 	if (x.nums[0]+x.inc)/max == 1 {
 		//this is so we don't have to add 1 to the if statment each loop
 		max -= 1
 		for i := range x.nums[1:] {
 			if x.nums[i+1] == max {
 				x.nums[i+1] = 0
-				x.current[i+1] = rns[0]
+				x.current[i+1] = (*rns)[0]
 			} else {
 				x.nums[i+1] += 1
-				x.current[i+1] = rns[x.nums[i+1]]
+				x.current[i+1] = (*rns)[x.nums[i+1]]
 				break
 			}
 		}
@@ -41,56 +51,53 @@ func (x PassTest) getNext(rns []rune) {
 		max += 1
 	}
 	x.nums[0] = (x.nums[0] + x.inc) % max
-	x.current[0] = rns[x.nums[0]]
+	x.current[0] = (*rns)[x.nums[0]]
 }
 
 func (x PassTest) toString() string {
 	return string(x.current)
 }
 
-func (z *PassTest) pTinit(sz, spot, threads int, rns []rune) {
-	x := PassTest{threads, make([]int, sz), make([]rune, sz)}
+func (z *PassTest) pTinit(sz, spot, threads int, rns *[]rune, pipe *chan string) {
+	x := PassTest{threads, make([]int, sz), make([]rune, sz), pipe}
 	for i := range x.current {
-		x.current[i] = rns[0]
+		x.current[i] = (*rns)[0]
 		x.nums[i] = 0
 	}
-	x.current[0] = rns[spot]
+	x.current[0] = (*rns)[spot]
 	x.nums[0] = spot
 	z.current = x.current
 	z.inc = x.inc
 	z.nums = x.nums
+	z.pipe = x.pipe
 }
 
 func GuessSingle(hash string, possibleRunes []rune) string {
 	//create a pipe to pass hashes to our workers
-	test := make(chan string, 100)
+	pipe := make(chan string, 100)
 	found := ""
 	//create the function that will generate plaintext passwords
 	func() {
-		defer close(test)
-		passSize := 3
-		possibleRunes := []rune{'a', 'b', 'c'}
-		var worker PassTest
-		worker.pTinit(passSize, 0, 3, possibleRunes)
-		var worker2 PassTest
-		worker2.pTinit(passSize, 1, 3, possibleRunes)
-		var worker3 PassTest
-		worker3.pTinit(passSize, 2, 3, possibleRunes)
-		for i := math.Pow(float64(len(possibleRunes)), float64(passSize)); i > 0; i -= 1 {
-			fmt.Println(worker.toString())
-			worker.getNext(possibleRunes)
-			fmt.Println(worker2.toString())
-			worker2.getNext(possibleRunes)
-			fmt.Println(worker3.toString())
-			worker3.getNext(possibleRunes)
-		}
+		defer close(pipe)
+		passSize := 4
+		possibleRunes := []rune{'a', 'b', 'c', 'd'}
+		workload := int(math.Pow(float64(len(possibleRunes)), float64(passSize)) / 3)
+		var generator PassTest
+		var generator2 PassTest
+		var generator3 PassTest
+		generator.pTinit(passSize, 0, 3, &possibleRunes, &pipe)
+		generator2.pTinit(passSize, 1, 3, &possibleRunes, &pipe)
+		generator3.pTinit(passSize, 2, 3, &possibleRunes, &pipe)
+		generator.produce(workload, &possibleRunes)
+		generator2.produce(workload, &possibleRunes)
+		generator3.produce(workload, &possibleRunes)
 		fmt.Println("Hello, World!")
 
 	}()
 	//for x threads spwan x-1 workers
 	//might change to x-2 workers but for now we will just do 1
 	go func() {
-		ret := worker(hash, test)
+		ret := worker(hash, pipe)
 		if ret != "" {
 			found = ret
 		}
